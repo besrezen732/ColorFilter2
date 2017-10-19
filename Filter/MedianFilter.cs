232ba39b564248edbc53a8bb8cc4bb3f8f52e2cb
Сергей <sergey.trusov.96@mail.ru>
2017-10-19 22:51:41 +0300

    работает но точки критически малы

--- Filter/MedianFilter.cs
+++ Filter/MedianFilter.cs
@@ -1,4 +1,6 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
+using System.Runtime.CompilerServices;
 
 namespace Filter
 {
@@ -8,31 +10,42 @@ namespace Filter
         {
             const int m = 3;
             const int n = 3;
-            const int dm = 3 / 2;
-            const int dn = 3 / 2;
+            const int dm = m / 2;
+            const int dn = n / 2;
             var width = sourseImage.Width;
             var height = sourseImage.Height;
 
             
             var resultColor = new Color();
 
-            if ((x - dm >= 0 && x + dm < width) && (y - dn >= 0 && y + dn < height)) // проверка на выход за дипазон картинки
+            if ((x - dm >= 0 && x + dm < width) && (y - dn >= 0 && y + dn < height)
+            ) // проверка на выход за диапазон картинки
             {
-                int r = 0, g = 0, b = 0;
-                for (var i = x - dm; i <= x + dm; i++)
+                int[] r = new int[m * n];
+                int[] g = new int[m * n];
+                int[] b = new int[m * n];
+               
+                int k = 0; // счетчик для заполнения массивов цветом;
+                for (var i = x - dm; i < x + dm; i++)
                 {
-                    for (var j = y - dn; j <= y + dn; j++)
+                    for (var j = y - dn; j < y + dn; j++)
                     {
+                        k++;
                         var sourceColor = sourseImage.GetPixel(i, j);
-                        r = r + sourceColor.R;
-                        g = g + sourceColor.G;
-                        b = b + sourceColor.B;
+                        r[k] = sourceColor.R;
+                        g[k] = sourceColor.G;
+                        b[k] = sourceColor.B;
                     }
                 }
-                resultColor = Color.FromArgb( Clamp((int)(r / (n * m)),0,255), Clamp((int)(g / (n * m)), 0, 255), Clamp((int)(b / (n * m)), 0, 255));
+                //Начинаем сортировку
+                Array.Sort(r);
+                Array.Sort(g);
+                Array.Sort(b);
+                resultColor = Color.FromArgb((int)r[(m * n) / 2], (int)g[m * n / 2], (int)b[m * n / 2]);
+
             }
-            else
-                resultColor = Color.FromArgb(sourseImage.GetPixel(x, y).R, sourseImage.GetPixel(x, y).G,
+
+            resultColor = Color.FromArgb(sourseImage.GetPixel(x, y).R, sourseImage.GetPixel(x, y).G,
                     sourseImage.GetPixel(x, y).B);
             return resultColor;
         }
